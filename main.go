59eb7cccd84df1c4adc7987b4ab2ed42e620b0e8
Igor Siguta <isihuta@gmail.com>
2024-03-07 16:25:42 -0700

    process_receipt and get_points endpoints; basic routing

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Item struct {
 	ShortDescription string `json:"shortDescription"`
 	Price            string `json:"price"`
@@ -12,4 +18,37 @@ type receipt struct {
 	PurchaseTime string `json:"purchaseTime"`
 	Items        []Item `json:"items"`
 	Total        string `json:"total"`
+}
+
+// TODO: Make a dictionary of {id:receipt} here!
+
+type ProcessReceiptResponse struct {
+	ID string `json:"id"`
+}
+
+type GetReceiptPointsResponse struct {
+	Points int32 `json:"points"`
+}
+
+func processReceipts(c *gin.Context) {
+
+	response := ProcessReceiptResponse{
+		ID: "123",
+	}
+	c.IndentedJSON(http.StatusAccepted, response)
+}
+
+func getReceiptPoints(c *gin.Context) {
+
+	response := GetReceiptPointsResponse{
+		Points: 16,
+	}
+	c.IndentedJSON(http.StatusAccepted, response)
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/receipts/process", processReceipts)
+	router.GET("/receipts/:id/points", getReceiptPoints)
+	router.Run("localhost:8088") // in case if you have something running on 8080 already.
 }
