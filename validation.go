d6d04b6756e8e67b14f34ecbf3392859eb9042d6
Igor Siguta <isihuta@gmail.com>
2024-03-11 23:29:30 -0600

    validation

--- validation.go
+++ validation.go
@@ -12,61 +12,57 @@ import (
 func validateReceipt(c *gin.Context) {
 	var r receipt
 	if err := c.ShouldBindJSON(&r); err != nil {
-		fmt.Printf("Could not validate JSON!\n")
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("Could not validate JSON:", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		c.Abort()
 		return
 	}
 
 	// Validate retailer name
-	if matched, _ := regexp.MatchString("^[\\w\\s\\-]+$", r.Retailer); !matched {
-		fmt.Printf("%v is invalid retailer name!\n", r.Retailer)
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid retailer name"})
+	if !regexp.MustCompile(`^[\w\s\-]+$`).MatchString(r.Retailer) {
+		fmt.Printf("%v is an invalid retailer name!\n", r.Retailer)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid retailer name"})
 		c.Abort()
 		return
 	}
 
 	// Validate total amount
-	if matched, _ := regexp.MatchString("^\\d+\\.\\d{2}$", r.Total); !matched {
-		fmt.Printf("%v is invalid total amount!\n", r.Total)
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid total amount"})
+	if !regexp.MustCompile(`^\d+\.\d{2}$`).MatchString(r.Total) {
+		fmt.Printf("%v is an invalid total amount!\n", r.Total)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid total amount"})
 		c.Abort()
 		return
 	}
 
-	//Validate items
-	description_compiled_regex, _ := regexp.Compile(`^[\\w\\s\\-]+$`)
-	price_compiled_regex, _ := regexp.Compile(`^\\d+\\.\\d{2}$`)
-
+	// Validate items
 	for _, item := range r.Items {
-		if matched := description_compiled_regex.MatchString(item.ShortDescription); !matched {
-			fmt.Printf("%v is invalid item's short description!\n", item.ShortDescription)
-			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid Short Description"})
+		if !regexp.MustCompile(`^[\w\s\-]+$`).MatchString(item.ShortDescription) {
+			fmt.Printf("%v is an invalid item's short description!\n", item.ShortDescription)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid short description"})
 			c.Abort()
 			return
 		}
 
-		if matched := price_compiled_regex.MatchString(item.Price); !matched {
-			fmt.Printf("%v is invalid item's price!\n", item.Price)
-			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid price!"})
+		if !regexp.MustCompile(`^\d+\.\d{2}$`).MatchString(item.Price) {
+			fmt.Printf("%v is an invalid item's price!\n", item.Price)
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid price"})
 			c.Abort()
 			return
 		}
 	}
 
-	_, dateErr := time.Parse("2006-01-02", r.PurchaseDate)
-	_, timeErr := time.Parse("15:04", r.PurchaseTime)
-
-	if dateErr != nil {
-		errorMessage, _ := fmt.Printf("%v is invalid purchase date!\n%v", r.PurchaseDate, dateErr.Error())
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": errorMessage})
+	// Validate purchase date
+	if _, err := time.Parse("2006-01-02", r.PurchaseDate); err != nil {
+		fmt.Printf("%v is an invalid purchase date!\n%v\n", r.PurchaseDate, err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid purchase date"})
 		c.Abort()
 		return
 	}
 
-	if timeErr != nil {
-		errorMessage, _ := fmt.Printf("%v is invalid purchase time!\n%v", r.PurchaseTime, timeErr.Error())
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": errorMessage})
+	// Validate purchase time
+	if _, err := time.Parse("15:04", r.PurchaseTime); err != nil {
+		fmt.Printf("%v is an invalid purchase time!\n%v\n", r.PurchaseTime, err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid purchase time"})
 		c.Abort()
 		return
 	}
